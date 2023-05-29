587df7bd9375be839afdefc1a461e81bcc55772c
arthurercan <arthurtaylan@hotmail.com>
2023-05-29 03:50:33 -0300

    first commit

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,44 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "NOVO HOMEM DE FERRO EM 3D",
+      "description": "MARVEL ANUNCIA NOVO HOMEM DE FERRO, CONFIRA",
+      "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+      "id":"2",
+      "title": "TODOS OS FILMES CANCELADOS DA MARVEL ATÉ HOJE!",
+      "description": "TODOS OS FILMES CANCELADOS DA MARVEL ATÉ HOJE!,CONFIRA",
+      "photoCover":"https://i.ytimg.com/vi/_eSxDHFM18c/maxresdefault.jpg"
+    },
+    {
+      "id":"3",
+      "title": "NOVO FILME DOS X-MEN ESTÁ EM PÓS PRODUÇÃO",
+      "description": "NOVO FILME DOS X-MEN ESTÁ EM PÓS PRODUÇÃO,CONFIRA",
+      "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+    },
+    {
+      "id":"4",
+      "title": "ÚLTIMA HORA: MARVEL STUDIOS DEFINE 4 DATAS DE LANÇAMENTO PARA 2024",
+      "description": "ÚLTIMA HORA: MARVEL STUDIOS DEFINE 4 DATAS DE LANÇAMENTO PARA 2024,CONFIRA",
+      "photoCover":"https://www.murphysmultiverse.com/wp-content/uploads/2021/09/2024.jpg"
+    },
+    {
+      "id":"5",
+      "title": "NOVAS SÉRIES ANUNCIADA NO DISNEY +",
+      "description": "NOVAS SÉRIES ANUNCIADA NO DISNEY +,CONFIRA",
+      "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+      "id":"6",
+      "title": "TEMPORADAS NOVAS CONFIRMADAS PELA MARVEL",
+      "description": "TEMPORADAS NOVAS CONFIRMADAS PELA MARVEL,CONFIRA",
+      "photoCover":"https://sm.ign.com/ign_br/screenshot/default/marvel-tv-netflix-card_saau.jpg"
+    },
+    {
+      "id":"7",
+      "title": "RUMORES SOBRE SÉRIE FOCADO EM PANTERA T'CHALLABLACK",
+      "description": "RUMORES SOBRE SÉRIE FOCADO EM PANTERA T'CHALLABLACK,CONFIRA",
+      "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    }
+  ]
